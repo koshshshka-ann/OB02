f2f6ddacb614d9f3460dd611b242905236f378ed
Anna Siryak <grafova.a.i@gmail.com>
2025-05-26 17:13:43 +0300

    Update access_levels.py
    - оставила защищенность только у параметров объекта
    - добавила геттеры и сеттер
    - переписала логику методов админа с у четом того, что список НЕ создается внутри класса Admin
    - изменила доступ к атрибутам с учетом их защищенности (через геттеры)

--- access_levels.py
+++ access_levels.py
@@ -9,35 +9,55 @@
 # 3. Инкапсуляция данных: Убедись, что атрибуты классов защищены от прямого доступа и модификации снаружи. Предоставь доступ к необходимым атрибутам через методы (например, get и set методы).
 
 class User:
-    def __init__(self, _user_id, _name):
-        self._user_id = _user_id
-        self._name = _name
+    def __init__(self, user_id, name):
+        self._user_id = user_id
+        self._name = name
         self._access = 'user'
 
+    def get_id(self):
+        return self._user_id
+
+    def get_name(self):
+        return self._name
+
+    def get_access(self):
+        return self._access
+
+    def set_name(self, new_name):
+        old_name = self._name
+        self._name = new_name
+        print(f"\nИмя сотрудника '{old_name}' успешно заменено на '{new_name}'")
+
 class Admin(User):
-    def __init__(self, _user_id, _name):
-        super().__init__(_user_id, _name)
+    def __init__(self, user_id, name):
+        super().__init__(user_id, name)
         self._access = 'admin'
-        self._user_list = []
 
-    def _add_user(self, _user_id, _name):
-        _added_user = User(_user_id, _name)
-        self._user_list.append(_added_user)
-        print(f"Пользователь '{_added_user._name}' добавлен в список сотрудников!")
+    def add_user(self, user_list, user):
+        user_list.append(user)
+        print(f"Пользователь '{user.get_name()}' добавлен в список сотрудников!")
 
-    def _remove_user(self, _user_id, _name):
-        _deleted_user = User(_user_id, _name)
-        self._user_list.remove(_deleted_user)
-        print(f"Пользователь '{_deleted_user._name}' удален из списка сотрудников!")
+    def remove_user(self, user_list, user):
+        user_list.remove(user)
+        print(f"Пользователь '{user.get_name()}' удален из списка сотрудников!")
 
-    def _info(self):
-        for user in self._user_list:
-            print(f"""\nСотрудник {user._user_id}:
-Имя: {user._name}
-Доступ: {user._access}""")
+    def info(self, user_list):
+        print("\n=== СПИСОК СОТРУДНИКОВ ===")
+        for user in user_list:
+            print(f"""\nСотрудник {user.get_id()}:
+Имя: {user.get_name()}
+Доступ: {user.get_access()}""")
 
+users = []
+
+user1 = User("u-01", "Николай Петров")
+user2 = User("u-02", "Екатерина Максимова")
 admin = Admin("a-01", "Иван Иванов")
 
-admin._add_user("u-01", "Николай Петров")
-admin._add_user("u-02", "Екатерина Максимова")
-admin._info()
+admin.add_user(users, user1)
+admin.add_user(users, user2)
+admin.add_user(users, admin)
+admin.info(users)
+
+user2.set_name("Екатерина Иванова")
+admin.info(users)
